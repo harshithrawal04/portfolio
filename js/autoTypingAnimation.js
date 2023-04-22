75aa33fe2c13a2328f22d5890c423d212270b2c6
sanjanaaa21 <asanjana159@gmail.com>
2023-04-23 01:19:36 +0530

    please happen now

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Harshiith Singh Rawal</span> As an engineering student, I've been trained in various aspects of science and technology, which has provided me with a well-rounded skill set. But what I'm truly passionate about is data analysis. I have always been fascinated by the power of data to transform businesses, inform decision-making, and drive innovation.<br>As a fresher in the field of data analysis, I am eager to learn and apply my skills in real-world scenarios. I am enthusiastic about exploring various techniques, such as data mining, predictive modeling, and machine learning, to draw insights from complex datasets.<br>My interest in data analysis has led me to develop a strong foundation in programming languages like Python and R, as well as tools like SQL and Tableau. I am comfortable working with big data and can manipulate and visualize it using various tools.<br>Apart from my technical skills, I believe my ability to communicate effectively and work collaboratively makes me a valuable asset to any team. I am always open to feedback and thrive in environments where I can learn and grow alongside my colleagues."
+      ],
+      typeSpeed: 1,
+      contentType: 'html'
+    });
+  }, 10);
